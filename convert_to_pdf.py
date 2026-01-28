8846e08abdf8d739a28a34c70ac584da960bc533
Lana <lananoor@microsoft.com>
2026-01-28 15:58:45 +0400

    remove file names

--- convert_to_pdf.py
+++ convert_to_pdf.py
@@ -19,10 +19,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Configuration
-INPUT_FOLDER = r"C:\Users\lananoor\OneDrive - Microsoft\ADIC\TranscriptionCode\outputRawTranscription"
-OUTPUT_FOLDER_LOCAL = r"C:\Users\lananoor\OneDrive - Microsoft\ADIC\TranscriptionCode\outputPDF"
+INPUT_FOLDER = ""
+OUTPUT_FOLDER_LOCAL = ""
 STORAGE_CONNECTION_STRING = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
-OUTPUT_CONTAINER = "adic-transcription-pdf"
+OUTPUT_CONTAINER = "transcription-pdf"
 
 # Ensure output folder exists
 os.makedirs(OUTPUT_FOLDER_LOCAL, exist_ok=True)
